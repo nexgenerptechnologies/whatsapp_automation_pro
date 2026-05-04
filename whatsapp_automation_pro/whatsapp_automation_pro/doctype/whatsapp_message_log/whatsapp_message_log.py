7d8eae24de48eb1c2d6d63cc6189310144b97e98
NexGen Enterprises <nexgenerptechnologies@gmail.com>
2026-05-04 17:33:14 +0530

    Fix: Add missing python controller files for all doctypes

---
+++ whatsapp_automation_pro/whatsapp_automation_pro/doctype/whatsapp_message_log/whatsapp_message_log.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, NexGen Enterprises and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class WhatsAppMessageLog(Document):
+	pass
